f673cc52326a776d4da8eb37037532e786511952
egrosscurt <84039948+egrosscurt@users.noreply.github.com>
2021-05-24 16:05:11 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Dit is een grote som
+console.log("Hallo Winc Academy studenten");
+/*let vermenigvuldiging = 1230941 * 1823794;
+console.log("1.", vermenigvuldiging);
+let deling = 637263 / 54;
+console.log("2.", deling);*/
+// Je ziet de regel comment niet omdat het een comment is en die hebben geen invloed op je code
+// Nee je ziet de uitkomst niet omdat deze geblokt is 
